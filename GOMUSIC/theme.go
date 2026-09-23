32379e4b44540cf99fe105977423f2789f13cc5d
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2026-09-23 21:42:37 +0530

    add Kanha theme strings and help pages

---
+++ GOMUSIC/theme.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"strings"
+)
+
+const (
+	btnAddMe     = " ᴇᴅᴅ мє тσ уσᴜр грσᴜп"
+	btnHelpStart = " нєʟп & сσммᴇηдs"
+	btnUpdates   = " ᴜпдᴇтєs"
+	btnSupport   = "sᴜппσрт"
+	btnSource    = " sσᴜрсє сσдє"
+	btnCommands  = " сσммᴇηдs"
+	btnClose     = "сʟσsє"
+	btnBack      = " бᴇск"
+	btnAdmin     = "ᴇдмɪη"
+	btnAuth      = "ᴇᴜтн"
+	btnBcast     = "б-сᴇsт"
+	btnPlay      = "пʟᴇу"
+	btnSudo      = "sᴜдσ"
+	btnRestrict  = "рєsтрɪст"
+	btnThumb     = "тнᴜмбηᴇɪʟ"
+	btnStart     = "sтᴇрт"
+	btnAutoplay  = "ᴇᴜтσпʟᴇу"
+	btnPlaylist  = "пʟᴇуʟɪsт"
+	btnVCLogs    = "вс-ʟσгs"
+	btnInline    = "ɪηʟɪηє"
+	btnPlayNow   = "пʟᴇу ησв"
+	btnSkip      = "sкɪп"
+	btnSeekBack  = "-𝟗𝟕ˢ"
+	btnSeekFwd   = "𝟗𝟕ˢ+"
+)
